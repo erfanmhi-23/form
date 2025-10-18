971623f3fdeddcd70d6de683ce120bb719a28e38
erfanmhi-23 <mhi.erfan.23@gmail.com>
2025-10-18 13:17:48 +0330

    database/setting

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts',
+    'form',
+    'conclusion',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appointment_db',
+        'USER': 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST':'db',
+        'PORT':5432,
+    
     }
 }
 
