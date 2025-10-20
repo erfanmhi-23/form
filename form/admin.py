934b5416701b94e0332dc8cfdcc5872725c005d9
zahramhld <139510503+zahramhld@users.noreply.github.com>
2025-10-20 18:35:59 +0330

    edit admin

--- form/admin.py
+++ form/admin.py
@@ -1,38 +1,26 @@
 from django.contrib import admin
-from .models import Category, Form, Text, SelectCheckbox, Rating, Process
+from .models import Category, Form, Process, Answer
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'create', 'update')
     search_fields = ('name',)
-    list_filter = ('create',)
+    list_filter = ('create','update')
 
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'force', 'question_num', 'view_count', 'create', 'update', 'answer_num')
+    list_display = ('id', 'title', 'category', 'force', 'question_num', 'view_count', 'create', 'update', 'answer_num', 'type', 'options')
     search_fields = ('title', 'question', 'description')
-    list_filter = ('category', 'force', 'question_num', 'create', 'answer_num')
-
-@admin.register(Text)
-class TextAdmin(admin.ModelAdmin):
-    list_display = ('id', 'form', 'text')
-    search_fields = ('text', 'form__title')
-    list_filter = ('form',)
-
-@admin.register(SelectCheckbox)
-class SelectCheckboxAdmin(admin.ModelAdmin):
-    list_display = ('id', 'form', 'multiple_choices', 'choices')
-    search_fields = ('choices', 'form__title')
-    list_filter = ('multiple_choices', 'form')
-
-@admin.register(Rating)
-class RatingAdmin(admin.ModelAdmin):
-    list_display  = ('id', 'form', 'rating', 'emoji')
-    search_fields = ('emoji', 'form__title')
-    list_filter   = ('rating', 'form')
+    list_filter = ('category', 'force', 'question_num', 'create', 'answer_num', 'type')
 
 @admin.register(Process)
 class ProcessAdmin(admin.ModelAdmin):
     list_display = ('id', 'form', 'liner', 'view_count')
     search_fields = ('form__title',)
     list_filter = ('liner', 'form')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'form', 'process', 'type', 'answer')
+    search_fields = ('answer', 'type', 'form__title')
+    list_filter   = ('type', 'form')
