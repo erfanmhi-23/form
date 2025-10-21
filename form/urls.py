784d7a0635078710e5d804ed8320f0c6dd5772ca
erfanmhi-23 <mhi.erfan.23@gmail.com>
2025-10-22 00:30:06 +0330

    show all list , show detail + urls

--- form/urls.py
+++ form/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from .views import FormListView, FormDetailView
+import views
+
 
 urlpatterns = [
-    path('forms/', views.form_list_create, name='form-list-create'),
-    path('forms/<int:form_id>/detail/', views.form_detail, name='form-detail'),
+    path('forms/', FormListView.as_view(), name='form-list'),
+    path('forms/<int:form_id>/detail/', FormDetailView.as_view(), name='form-detail'), 
     path('forms/<int:form_id>/update/', views.form_update, name='form-update'),
     path('forms/<int:form_id>/delete/', views.form_delete, name='form-delete'),
 
--- form/views.py
+++ form/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Form
+from .serializers import FormSerializer
 
-# Create your views here.
+class FormListView(APIView):
+    def get(self, request):
+        forms = Form.objects.all()
+        serializer = FormSerializer(forms, many=True)
+        return Response(serializer.data)
+
+class FormDetailView(APIView):
+    def get(self, request, form_id):
+        try:
+            form = Form.objects.get(id=form_id)
+        except Form.DoesNotExist:
+            return Response({'error': 'Form not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = FormSerializer(form)
+        return Response(serializer.data)
