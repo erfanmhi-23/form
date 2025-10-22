74e8f0bbef65255f5a4ce265a0eeea786ede600c
hossein <hossein.zaferani1381@gmail.com>
2025-10-22 12:00:32 +0330

    view_serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from accounts.models import User
+
+
+User = get_user_model()
+
+class UserSerializer(ModelSerializer) :
+    class Meta : 
+        model = User
+        fields = ('id','username','email','first_name','last_name')
+    
+
+class DeleteAccountSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class OTPVerifySerializer(serializers.Serializer):
+    code = serializers.CharField(max_length=10)
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username','email','password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
